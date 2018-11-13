09ab953701272c1f8f70f097c2896003c7b3fb6f
aurumvorax <aurumvorax@gmail.com>
2018-11-13 15:50:32 -0800

    Last bugs squashed in Inventory.  Basic shipyard is functionally complete.

--- core/src/aurumvorax/arcturus/inventory/Inventory.java
+++ core/src/aurumvorax/arcturus/inventory/Inventory.java
@@ -5,11 +5,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.IntMap;
 
 public class Inventory extends VerticalGroup implements Draggable.Target{
 
     private Array<Item.Stack> items = new Array<>();
-    private Array<InventoryEntry> listEntries = new Array<>();
+    private IntMap<InventoryEntry> listEntries = new IntMap<>();
+    private IntMap<DragSource> listSources = new IntMap<>();
     private DragAndDrop draganddrop;
     private Skin skin;
 
@@ -28,22 +30,46 @@ public class Inventory extends VerticalGroup implements Draggable.Target{
 
     @Override
     public int add(Item.Stack stack){
-        int idx = items.indexOf(stack, false);
+        int idx = items.indexOf(stack, false);  // Relies on the fact that Item.Stack.equals() ignores quantity
+
+        // don't already have it
+            //add to items
+            // new listentry
+
+        // do already have it
+            //update items
+            //updatelistentry
+
+        // confirming empty
+            //remove from items
+            //remove listentry
 
         if((stack.quantity == 0) && (listEntries.get(idx).getStack().quantity == 0)){
-            listEntries.removeIndex(idx);
-            update();
+            // Confirm removal or empty entry
+            items.removeIndex(idx);
+            this.removeActor(listEntries.get(idx));
+            listEntries.remove(idx);
+            draganddrop.removeSource(listSources.get(idx));
+            listSources.remove(idx);
             return 0;
         }
 
         if(idx == -1){
+            // Create new entry
+            idx = items.size;
             items.add(new Item.Stack(stack));
-            listEntries.add(new InventoryEntry(new Item.Stack(stack), this, skin));
-            update();
-        }else{
-            items.get(idx).quantity += stack.quantity;
-            listEntries.get(idx).update(items.get(idx));
+            InventoryEntry newEntry = new InventoryEntry(stack, this, skin);
+            listEntries.put(idx, newEntry);
+            this.addActor(newEntry);
+            DragSource newSource = new DragSource(newEntry);
+            listSources.put(idx, newSource);
+            draganddrop.addSource(newSource);
+            return 0;
         }
+
+        // Update existing entry
+        items.get(idx).quantity += stack.quantity;
+        listEntries.get(idx).update(items.get(idx));
         return 0;
     }
 
@@ -60,11 +86,14 @@ public class Inventory extends VerticalGroup implements Draggable.Target{
 
     public Actor update(){
         clearChildren();
-        draganddrop.addTarget(new DragTarget(this));
-        for(InventoryEntry entry : listEntries){
+
+        for(InventoryEntry entry : listEntries.values())
             addActor(entry);
-            draganddrop.addSource(new DragSource(entry));
-        }
+
+        draganddrop.addTarget(new DragTarget(this));
+        for(DragSource source : listSources.values())
+            draganddrop.addSource(source);
+
         return this;
     }
 }
