6a432071daaa4fabf1de3f8ac517dad5702b87c3
aurumvorax <aurumvorax@gmail.com>
2018-11-20 16:10:50 -0800

    Moved unpacked images out of the classpath and built separate tool for packing texture atlasses

--- tools/src/main/TextureAtlasPacker.java
+++ tools/src/main/TextureAtlasPacker.java
@@ -1,8 +1,21 @@
 package main;
 
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
 public class TextureAtlasPacker{
 
-    public static void Main(String args[]){
+    public static void main(String args[]){
+
+
+        TexturePacker.Settings settings = new TexturePacker.Settings();
+        settings.maxHeight = 2048;
+        settings.maxWidth = 2048;
+        settings.edgePadding = false;
+        settings.combineSubdirectories = true;
+        settings.flattenPaths = true;
+        TexturePacker.process(settings, "unpacked/img/sprites", "core/assets/img", "SpriteAtlas");
+        TexturePacker.process(settings, "unpacked/img/animations", "core/assets/img", "AnimationAtlas");
+        TexturePacker.process(settings, "unpacked/img/backgrounds", "core/assets/img", "BackgroundAtlas");
 
     }
 }
