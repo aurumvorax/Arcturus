26cbfb7d427017e0e79f15a7f85ea5a359d3d862
aurumvorax <aurumvorax@gmail.com>
2018-12-02 00:18:28 -0800

    System change working, using basic hyperdrive.

--- core/src/aurumvorax/arcturus/galaxy/SolarSystemManager.java
+++ core/src/aurumvorax/arcturus/galaxy/SolarSystemManager.java
@@ -4,6 +4,8 @@ import aurumvorax.arcturus.artemis.factories.OrbitalData;
 import aurumvorax.arcturus.artemis.factories.ShipData;
 import aurumvorax.arcturus.artemis.factories.ShipFactory;
 import aurumvorax.arcturus.artemis.factories.StellarFactory;
+import aurumvorax.arcturus.artemis.systems.PlayerShip;
+import aurumvorax.arcturus.screens.GameScreen;
 import aurumvorax.arcturus.services.EntityData;
 
 import java.util.HashMap;
@@ -11,9 +13,11 @@ import java.util.HashMap;
 public class SolarSystemManager{
 
     private static String currentSystem;
+    private static String navigationTarget = null;
     private static HashMap<String, StellarData.Extra> solarSystems = new HashMap<>();
 
 
+    public static void setNavigationTarget(String target){ navigationTarget = target; }
     public static String getCurrentSystem(){ return currentSystem; }
 
     public static void loadSystem(String systemName){
@@ -36,5 +40,19 @@ public class SolarSystemManager{
     public static void saveSystem(){
         StellarData.Extra save = StellarFactory.extractCurrentSystem();
         solarSystems.put(currentSystem, save);
+    }
+
+    public static void hyperdrive(){
+        if((navigationTarget == null) || (navigationTarget.equals(currentSystem)))
+            return;
+
+        PlayerShip.extract();
+        StellarData.Extra save = StellarFactory.extractCurrentSystem();
+        solarSystems.put(currentSystem, save);
+
+        GameScreen.resetWorld();
+
+        PlayerShip.insert();
+        loadSystem(navigationTarget);
     }
 }
