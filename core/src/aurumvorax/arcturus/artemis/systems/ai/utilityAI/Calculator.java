4ba85bbcec7c6b65f80e9e775a38e186b9274db4
aurumvorax <aurumvorax@gmail.com>
2019-05-03 17:44:17 -0700

    Started implementation of a utility function based AI framework.  Input transforms complete but untested.

---
+++ core/src/aurumvorax/arcturus/artemis/systems/ai/utilityAI/Calculator.java
@@ -0,0 +1,9 @@
+package aurumvorax.arcturus.artemis.systems.ai.utilityAI;
+
+import com.badlogic.gdx.math.MathUtils;
+
+public class Calculator{
+
+
+
+}
---
+++ core/src/aurumvorax/arcturus/artemis/systems/ai/utilityAI/CoreBuilder.java
@@ -0,0 +1,28 @@
+package aurumvorax.arcturus.artemis.systems.ai.utilityAI;
+
+public class CoreBuilder extends Core{
+
+    public CoreBuilder(){
+        super();
+    }
+
+    public CoreBuilder addAction(){
+
+        return this;
+    }
+
+    public CoreBuilder addActionCluster(){
+
+        return this;
+    }
+
+    public CoreBuilder addInput(Input input){
+
+        return this;
+    }
+
+    public CoreBuilder addInputCluster(){
+
+        return this;
+    }
+}
